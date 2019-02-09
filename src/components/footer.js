14b8eb390d8f7a3267684e803b05d9f4d341c75d
GopeshMedayil <medayilgopesh@gmail.com>
2019-02-09 21:19:39 +0530

    WIP gatsby integration

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => (
+
+    <React.Fragment>
+        <footer id="main-footer">
+            Copyright &copy; 2018
+        </footer>
+    </React.Fragment>
+
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,34 @@
 import React from "react";
 import '../../static/dist/css/main.css'
-import Header from '../components/header.js';
+import Layout from '../components/layout.js'
 var FA = require('react-fontawesome')
 
 export default () => (
     <div>
-        <Header />
-        <main id="home">
-            <h1 className="lg-heading">
-                Gopesh <span className="text-secondary">Gopinath</span>
-            </h1>
-            <h2 className="sm-heading">
-                Front end developer &hearts; Web
+        <Layout>
+            <main id="home">
+                <h1 className="lg-heading">
+                    Gopesh <span className="text-secondary">Gopinath</span>
+                </h1>
+                <h2 className="sm-heading">
+                    Front end developer &hearts; Web !!
         </h2>
-            <div className="icons">
-                <a href="https://twitter.com/GopeshMedayil">
-                    <i className="fab fa-twitter fa-2x"></i>
-                </a>
-                <a href="https://www.facebook.com/gopesh.gopinath">
-                    <FA name="rocket" />
-                    {/* <i className="fab fa-facebook fa-2x"></i> */}
-                </a>
-                <a href="www.linkedin.com/in/gopesh-gopinath">
-                    <i className="fab fa-linkedin fa-2x"></i>
-                </a>
-                <a href="https://github.com/GopeshMedayil">
-                    <i className="fab fa-github fa-2x"></i>
-                </a>
-            </div>
-        </main>
+                <div className="icons">
+                    <a href="https://twitter.com/GopeshMedayil">
+                        <i className="fab fa-twitter fa-2x"></i>
+                    </a>
+                    <a href="https://www.facebook.com/gopesh.gopinath">
+                        <FA name="rocket" />
+                        {/* <i className="fab fa-facebook fa-2x"></i> */}
+                    </a>
+                    <a href="www.linkedin.com/in/gopesh-gopinath">
+                        <i className="fab fa-linkedin fa-2x"></i>
+                    </a>
+                    <a href="https://github.com/GopeshMedayil">
+                        <i className="fab fa-github fa-2x"></i>
+                    </a>
+                </div>
+            </main>
+        </Layout>
     </div>
 );
