1e3ea35a390aff7567fecfb28a28fac0da88fc6c
GopeshMedayil <medayilgopesh@gmail.com>
2019-04-02 23:30:02 +0530

    Fixed issue on icon size in firefox

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,18 +21,18 @@ export default () => (
                 <div className="icons">
                     <a href="https://twitter.com/GopeshMedayil">
                         {/* <i className="fab fa-twitter fa-2x"></i> */}
-                        <FaTwitter size="2rem" />
+                        <FaTwitter size="2em" />
                     </a>
                     <a href="https://www.facebook.com/gopesh.gopinath">
-                        <FaFacebook size="2rem" />
+                        <FaFacebook size="2em" />
                         {/* <FA name="rocket" /> */}
                         {/* <i className="fab fa-facebook fa-2x"></i> */}
                     </a>
                     <a href="https://www.linkedin.com/in/gopesh-gopinath">
-                        <FaLinkedin size="2rem" />
+                        <FaLinkedin size="2em" />
                     </a>
                     <a href="https://github.com/GopeshMedayil">
-                        <FaGithub size="2rem" />
+                        <FaGithub size="2em" />
                     </a>
                 </div>
             </main>
